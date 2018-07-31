83d9ba24ab2c62d9d4a41800d3e0444f1c6ec600
b-runz <runz10@hotmail.com>
2018-07-31 23:12:43 +0200

    Tested out app-engine.

---
+++ lister-web/src/main/kotlin/dk/runz/web/App.kt
@@ -0,0 +1,7 @@
+package dk.runz.web
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+@SpringBootApplication
+open class App : SpringBootServletInitializer()
